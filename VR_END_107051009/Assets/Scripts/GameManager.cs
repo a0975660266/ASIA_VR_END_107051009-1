b0ebda847085371cdc6017925161dbb29ec22c20
a0975660266 <71679957+a0975660266@users.noreply.github.com>
2021-01-12 22:13:01 +0800

    完成

---
+++ VR_END_107051009/Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("丟垃圾");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- VR_END_107051009/Assets/Scripts/ScoreManager.cs
+++ VR_END_107051009/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
     {
-        
+        //音效來源 = 取得元件<音效來源>
+        aud = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.tag == "垃圾" )
+        {
+            AddScore();
+        }
+        //如果 碰撞的根物件名稱為Player
+        if (other.transform.root.name == "Player")
+        {
+            //玩家進入三分區域，將投進的分數改為三分
+            scoreIn = 3;
+        }
     }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player")
+        {
+            //玩家離開三分區域，將投進的分數改為二分
+            scoreIn = 2;
+        }
+    }
+
+    ///<summary>
+    ///加分數
+    ///</summary>
+    private void AddScore()
+    {
+        score += scoreIn;                                                                               //分數遞增  投進的分數
+        textScore.text = "分數 :" + score;                                                  //更新介面
+        aud.PlayOneShot(soundIn, Random.Range(1f, 3f));
+    }
+
 }
